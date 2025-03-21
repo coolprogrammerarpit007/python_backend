6820f4d26f4d4f9cc54f40e4f4a04d447711b37b
arpit mishra <arpit.mishra.out@gmail.com>
2025-03-21 07:59:30 +0530

    Started Backend, currently on python fundamentals

---
+++ python/main.py
@@ -0,0 +1,14 @@
+# print("I am learning backend with python!")
+# print("Currently learning python then a framework called Django")
+
+name = "Arpit"
+age = 25
+is_married = False
+
+# print(f"My name is {name}, I am {age} years old! and i am {'married' if is_married else 'not married'}")
+
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+area_of_rectangle = length * width
+# print(f"Area of rectangle: {area_of_rectangle}")
